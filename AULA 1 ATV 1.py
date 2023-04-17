6d5d5ffe97b82a3a7ecf291e29461c9cc2766243
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-17 13:13:07 -0300

    Create AULA 1 ATV 1.py

---
+++ AULA 1 ATV 1.py
@@ -0,0 +1,7 @@
+n1 = float(input("Informe o primeiro número: "))
+n2 = float(input("Informe o segundo número: "))
+n3 = float(input("Informe o terceiro número: "))
+
+media = (n1 + n2 + n3) / 3
+
+print("A média dos três números é: ", media)
