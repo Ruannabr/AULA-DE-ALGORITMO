3cd0387455bb563db3945db6e1d53471be11b230
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:53:10 -0300

    Create EXTRA4.py

---
+++ EXTRA4.py
@@ -0,0 +1,9 @@
+# Escreva um programa que leia o valor do salário de 
+# um funcionário e 
+# calcule o novo salário com um aumento de 15%.
+salario = float(input("digite salario"))
+# porcentagem = 15/100
+# aumento = salario * porcentagem
+# novosalario = salario + aumento
+novosalario = salario * 1.15
+print("o novo salario é: ",novosalario)
