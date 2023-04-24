ca4463deb9070c807552daafe4dcd2db16b42822
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:53:32 -0300

    Create EXTRA5.py

---
+++ EXTRA5.py
@@ -0,0 +1,9 @@
+# Escreva um programa que leia dois números inteiros do usuário e
+# exiba na tela o maior deles.
+
+n1 = int(input("entre c um valor: "))  
+n2 = int(input("entre c outro valor: "))
+if n1 > n2:
+    print("o maior é: ", n1)
+else:
+    print("o maior é: ",n2)
