abbc16cd6e03a388aeab87cf1119408f5af2a867
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:06:43 -0300

    Create AULA 1 ATV 1.4.py

---
+++ AULA 1 ATV 1.4.py
@@ -0,0 +1,8 @@
+n = int (input ("Insira o número "))
+if n <=0:
+    print (f"O número {n} é neutro")
+elif n % 2 == 0:
+    print (f"O número {n} é par")
+
+else:
+    print (f"O número {n} é impár")
