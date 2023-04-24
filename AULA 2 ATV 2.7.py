9c9eb9a82ee930835b6fb3b2bbf986a0103cc53e
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:18:05 -0300

    Update AULA 2 ATV 2.7.py

--- AULA 2 ATV 2.7.py
+++ AULA 2 ATV 2.7.py
@@ -1,10 +1,6 @@
-num = int(input("Escolha um número: "))
-if num < 2:
-    print(num, "Não é um número primo.")
-else:
-    for i in range(2, num):
-        if num % i == 0:
-            print(num, "Esse número não é primo.")
-            break
-    else:
-         print(num, "Esse número é primo.") 
+n = int (input ("Digite o número: "))
+fatorial = 1
+for i in range (1, n+1):
+    if n > 0:
+        fatorial *= i
+        print (fatorial)
