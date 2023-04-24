b858bac0c1d384d882f00213eef2be070597ff6e
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:53:56 -0300

    Create EXTRA6.py

---
+++ EXTRA6.py
@@ -0,0 +1,11 @@
+# <18 menor de idade
+# acima de 18 e abaixo que 60 aulto
+# acima de 60 idoso
+idade = int(input("digite sua idade"))
+
+if idade < 18:
+    print("menor de idade")
+elif idade  >=18 and  idade < 60:
+    print("adulto")
+else:
+    print("idoso")
