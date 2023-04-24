1bb09e83f5e0619cd667ed3229687b057b692171
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:14:56 -0300

    Update AULA 2 ATV 2.3.py

--- AULA 2 ATV 2.3.py
+++ AULA 2 ATV 2.3.py
@@ -1,2 +1,7 @@
-for i in range(1, 11):
-     print(i)
+n1 = int (input ("Insira o número: "))
+n2 = int (input ("Insira o número: "))
+n3 = int (input ("Insira o número: "))
+n4 = int (input ("Insira o número: "))
+n5 = int (input ("Insira o número: "))
+media = (n1+n2+n3+n4+n5)/5
+print (f"A média é: {media}")
