3324f2cd9f1ac921133a0eb917374b09e3887164
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:16:42 -0300

    Update AULA 2 ATV 2.5.py

--- AULA 2 ATV 2.5.py
+++ AULA 2 ATV 2.5.py
@@ -1,6 +1,8 @@
-v1 = float(input("Digite o primeiro número: "))
-v2 = float(input("Digite o segundo número: "))
-v3 = float(input("Digite o terceiro número: "))
-v4 = float(input("Digite o quarto número: "))
-v5 = float(input("Digite o quinto número: "))
-print ((v1+v2+v3+v4+v5)/2)
+n = int (input ("Insira o número: "))
+if n == 2:
+    print (f"O número {n} é primo")
+elif n % 2 == 0:
+    print (f"O número {n} não é primo")
+
+else:
+    print (f"O número {n} é primo")
