9b45e381a5c4bafb32bd5c99881d14c1456d51ca
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:52:33 -0300

    Update EXTRA3.py

--- EXTRA3.py
+++ EXTRA3.py
@@ -1,8 +1,17 @@
-var = int(input("entre c um valor"))
-# var = 10
-# % mod é o resto da divisao
-if var%2 == 0:
-    print("par")
-else:
-    print("impar")
-        
+# algoritmo
+#     inteiro n, i
+#     n <- 100
+#     i <- 1
+#     enquanto i <= n faca
+#         escreva(i)
+#         i <- i + 1
+#     fim_enquanto
+# fim_algoritmo
+# =(atribuicao) e diferente de ==(igualdade)
+
+n = 100
+i = 1
+while i <= n:
+    print(i)
+    i = i+1
+
