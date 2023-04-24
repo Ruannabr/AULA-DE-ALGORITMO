e4f0e8d9370f3ff80a888b649a826581dcb3b165
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:34:06 -0300

    Create AULA 4  ATV 4.9.py

---
+++ AULA 4  ATV 4.9.py
@@ -0,0 +1,12 @@
+n1 = int (input("Valor inteiro 1: "))
+n2 = int (input("Valor inteiro 2: "))
+n3 = float (input ("Valor real: "))
+#a)
+a = (n1*2)*(n2/2)
+print ("Letra a", a)
+#b)
+b = (n1*3)+n3
+print ("Letra b", b)
+#c)
+c = n3**3
+print ("Letra c", c)
