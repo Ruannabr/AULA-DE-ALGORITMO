45651a0f243f43fe77bba3db1dc120288d84a1b7
Ruannabr <131011722+Ruannabr@users.noreply.github.com>
2023-04-24 13:17:25 -0300

    Update AULA 2 ATV 2.6.py

--- AULA 2 ATV 2.6.py
+++ AULA 2 ATV 2.6.py
@@ -1,3 +1,6 @@
-num = float(input("Escolha um numero: "))
-for i in range(1,11):
-    print(num, 'x', i, '=', num*i)
+n = int(input("Digite até que número determinado: "))
+
+for i in range(n+1):
+    potencia = 2 ** i
+    if potencia > n:
+        print (f"2 elevado à {i} = {potencia}")
